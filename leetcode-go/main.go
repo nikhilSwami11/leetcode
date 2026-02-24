c9a28264be9ce07733194fd7b130502a91f4ac4c
nikhilSwami11 <nikhilswamidevs@gmail.com>
2026-02-24 13:50:42 -0700

    basic assignments

--- leetcode-go/main.go
+++ leetcode-go/main.go
@@ -1,25 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+)
 
-func containsDuplicate(nums []int) bool {
-	seen := make(map[int]int)
+var ErrTruckNotFound = errors.New("truck not found")
 
-	for i := 0; i < len(nums); i++ {
-		_, found := seen[nums[i]]
-		if found {
-			return true
-		}
-		seen[nums[i]] = i
-	}
-	return false
+type FleetManager interface {
+	AddTruck(id string, cargo int) error
+	GetTruck(id string) (*Truck, error)
+	RemoveTruck(id string) error
+	UpdateTruckCargo(id string, cargo int) error
 }
 
-func main() {
-	nums := []int{2, 7, 11, 15}
+func (tm *truckManager) AddTruck(id string, cargo int) error {
 
-	result := containsDuplicate(nums)
-	fmt.Println("Result:", result)
+	tm.trucks[id] = &Truck{ID: id, Cargo: cargo}
+	return nil
+}
+
+func (tm *truckManager) GetTruck(id string) (*Truck, error) {
+	value, exists := tm.trucks[id]
+	if !exists {
+		return nil, ErrTruckNotFound
+	}
+	return value, nil
+}
+
+func (tm *truckManager) RemoveTruck(id string) error {
+	_, exists := tm.trucks[id]
+	if !exists {
+		return ErrTruckNotFound
+	}
+	delete(tm.trucks, id)
+	return nil
+}
+
+func (tm *truckManager) UpdateTruckCargo(id string, cargo int) error {
+	_, exists := tm.trucks[id]
+	if !exists {
+		return ErrTruckNotFound
+	}
+	tm.trucks[id].Cargo = cargo
+	return nil
+}
+
+type Truck struct {
+	ID    string
+	Cargo int
+}
+
+type truckManager struct {
+	trucks map[string]*Truck
+}
+
+func NewTruckManager() truckManager {
+	return truckManager{
+		trucks: make(map[string]*Truck),
+	}
 }
 
 //Solution for Two SUM problem
@@ -36,4 +74,18 @@ func main() {
 // 	}
 
 // 	return []int{}
+// }
+
+// Solution for Contains Duplicate
+// func containsDuplicate(nums []int) bool {
+// 	seen := make(map[int]int)
+
+// 	for i := 0; i < len(nums); i++ {
+// 		_, found := seen[nums[i]]
+// 		if found {
+// 			return true
+// 		}
+// 		seen[nums[i]] = i
+// 	}
+// 	return false
 // }
