b5fa6d3b1854e026495739273b322757895bcb9c
nikhilSwami11 <nikhilswamidevs@gmail.com>
2026-04-12 14:06:51 -0700

    k smallest solved

--- quest/sequence_shoal/heap/k_smallest.py
+++ quest/sequence_shoal/heap/k_smallest.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 class Solution(object):
     def kSmallestPairs(self, nums1, nums2, k):
         """
@@ -6,11 +9,21 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        i = 0
-        j = 0
-        while i+1 < len(nums1) and j+1 < len(nums2):
-            if nums1[i] + nums2[j] < nums1[i + 1] + nums2[j]:
-                pass
+        min_heap = []
+        n = min(len(nums1),k)
+        for i in range(n):
+            min_heap.append((nums1[i]+nums2[0],i,0))
+        heapq.heapify(min_heap)
+
+        results = []
+        while k:
+           curr_sum, i, j = heapq.heappop(min_heap)
+           results.append([nums1[i],nums2[j]])
+           if (j+1) < len(nums2):
+            heapq.heappush(min_heap,(nums1[i]+nums2[j+1], i, j+1))
+           k -=1
+        
+        return results
 
 nums1 = [1,7,11]
 nums2 = [2,4,6]
