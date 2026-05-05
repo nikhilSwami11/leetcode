6a1c763df0cab9f72ef27f81878078f82cd5383a
nikhilSwami11 <nikhilswamidevs@gmail.com>
2026-05-05 14:03:15 -0700

    completed reverse list

--- quest/associated_slope/linked_list/odd_even_reverse.py
+++ quest/associated_slope/linked_list/odd_even_reverse.py
@@ -30,7 +30,18 @@ class Solution(object):
         
         odd_start.next = even_start_fix
         return head
-
+    
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        prev = None
+        curr = head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
         
     
 def create_linked_list(ls: List[int]):
