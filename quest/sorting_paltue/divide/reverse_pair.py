25541398c651202d4b56a84075d5d1d40345bd24
nikhilSwami11 <nikhilswamidevs@gmail.com>
2026-06-08 18:55:29 -0700

    todays commit

--- quest/sorting_paltue/divide/reverse_pair.py
+++ quest/sorting_paltue/divide/reverse_pair.py
@@ -3,5 +3,14 @@ from typing import List
 
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-        pass
+        count = 0
+        for i in range(len(nums)-1,-1,-1):
+            for j in range(i-1, -1,-1):
+                if 2*nums[i]<nums[j]:
+                    count+=1
+        
+        return count
+
+nums = [2,4,3,5,1]
+print(Solution().reversePairs(nums))
 
