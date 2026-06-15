445f18ebb3e09357669f84e524f0a2ab1ba60227
nikhilSwami11 <nikhilswamidevs@gmail.com>
2026-06-14 23:54:06 -0700

    new updated

---
+++ quest/recusion_maze/sliding_window/substring.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        abc_set = {}
+        start = 0
+        n = len(s)
+        total_substring = 0
+        i = 0
+        for i in range(n):
+            abc_set[s[i]] = abc_set.get(s[i], 0) + 1
+
+            while len(abc_set) == 3:
+                total_substring += n - i
+                abc_set[s[start]] -= 1
+            
+                if abc_set[s[start]] == 0:
+                    del abc_set[s[start]]
+                start += 1
+        return total_substring
+
+# done
+
+s = "aaacb"
+print(Solution().numberOfSubstrings(s))
